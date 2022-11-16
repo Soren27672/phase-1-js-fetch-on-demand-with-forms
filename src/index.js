bdda3c4f096415fdc42b23c06196233b4761a520
Maxwell Gottesman <maxgman16@yahoo.com>
2022-11-15 19:02:55 -0500

    commit tree c41c2b6ea72efd82e77f5e1a90552ba478261c05

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const init = () => {
-  
+    
+    const form = document.querySelector('form');
+    const returnTitle = document.getElementById('returnTitle');
+    const returnSummary = document.getElementById('returnSummary');
+    
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        fetch(`http://localhost:3000/movies/${form.querySelector('input').value}`)
+        .then(res => {
+            if (res.ok) return res.json();
+            else return {
+                title: "Movie Not Found",
+                summary: "",
+            }
+        })
+        .then(json => {
+        returnTitle.textContent = json.title;
+        returnSummary.textContent = json.summary;
+        });
+        
+        
+})
 }
 
 document.addEventListener('DOMContentLoaded', init);
